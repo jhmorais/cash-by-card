b82f955795394f49adf3f5be883dc99ecddc6ae4
joão morais <jhmorais.09@gmail.com>
2023-12-18 15:46:15 +0000

    Fix update fields

--- internal/ports/input/update_client.go
+++ internal/ports/input/update_client.go
@@ -3,7 +3,7 @@ package input
 type UpdateClient struct {
 	ID        int    `json:"id,omitempty"`
 	Name      string `json:"name"`
-	PixType   int    `json:"pix_type"`
-	PixKey    string `json:"pix_key"`
-	PartnerID int    `json:"partner_id"`
+	PixType   int    `json:"pixType"`
+	PixKey    string `json:"pixKey"`
+	PartnerID int    `json:"partnerId"`
 }
